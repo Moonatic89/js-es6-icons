6d1c26d0fbfcd29448bbc5ed2883dfb09ea54ce1
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-10-26 17:33:19 +0200

    Second milestone folder created

---
+++ milestone2/asset/js/main.js
@@ -0,0 +1,121 @@
+/*
+M1: From the data structure, show every icon in the document.
+M2: Color the icons per color
+M3: Use a select to filter icons via icon type
+
+
+MILESTONE 1
+
+*/
+
+
+const icons = [
+    {
+        name: 'cat',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'crow',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dog',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dove',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dragon',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'horse',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'hippo',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'fish',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'carrot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'apple-alt',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'lemon',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'pepper-hot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'user-astronaut',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-graduate',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-ninja',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-secret',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    }
+];
+
+
+icons.forEach((icon, i, array) => {
+    const uppercaseName = icon.name.toUpperCase();
+    const element = `
+    <div class="card">
+    <i class="${icon.family} ${icon.prefix}${icon.name} fa-2x"></i>
+    <p>${uppercaseName}</p>
+    </div>
+    `
+    document.querySelector(".cards").insertAdjacentHTML("beforeend", element)
+})
